b008f48f3b35b65e16c8d055a94008ba98bf8c0e
stas <voironlol@gmail.com>
2022-10-07 19:09:51 +0300

    [ADD] User subscribe and navigate to user from messages

--- src/components/Messages/UserMessage/UserMessage.tsx
+++ src/components/Messages/UserMessage/UserMessage.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react';
 import styles from './UserMessage.module.scss'
 import avatar from '../../../assets/UI/AvatarUser.png'
+import { useNavigate } from "react-router-dom";
+import { USERS } from "../../../constants/nameRoutesConsts";
 
 export interface UserMessageProps {
     name: string,
@@ -12,14 +14,21 @@ export interface UserMessageProps {
 
 export const UserMessage: FC<UserMessageProps> = ({name, text, index, isOpenMessages, setOpenMessages}) => {
 
+    const navigate = useNavigate()
+
+    const navigateToUser = (e: React.MouseEvent<HTMLImageElement> | React.MouseEvent<HTMLSpanElement>) => {
+        e.stopPropagation()
+        navigate(`${USERS}/st.koryk`)
+    }
+
     return (
         <div className={index === isOpenMessages ? `${styles.userMessageBlock} ${styles.active}`: styles.userMessageBlock} onClick={() => setOpenMessages(index)}>
             <div className={styles.avatar}>
-                <img src={avatar} alt="avatar"/>
+                <img src={avatar} alt="avatar" onClick={(e) => navigateToUser(e)}/>
             </div>
             <div className={styles.shortLastMessageBlock}>
                 <div className={styles.timeLastMessageBlock}>
-                    <span className={styles.name}>{name}</span>
+                    <span className={styles.name} onClick={(e) => navigateToUser(e)}>{name}</span>
                     <span className={styles.timeLastMessage}>3 minutes ago</span>
                 </div>
                 <div className={styles.shortLastMessage}>
--- src/components/User/UserHeader/UserHeader.tsx
+++ src/components/User/UserHeader/UserHeader.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './UserHeader.module.scss'
 import avatar from "../../../assets/UI/AvatarUser.png";
 import { Button, ButtonBack, IconElement } from "../../../components-ui";
 import likes from "../../../assets/Profile/Like.svg";
 import reposts from "../../../assets/Profile/Repost.svg";
 import views from "../../../assets/Profile/View.svg";
+import { useNavigate } from "react-router-dom";
+import { MESSAGES } from "../../../constants/nameRoutesConsts";
 
 export const UserHeader = () => {
+
+    const navigate = useNavigate()
+
+    const [isSubscribe, setSubscribe] = useState(false)
+
     return (
         <div className={styles.headerBlock}>
             <div className={styles.avatarBlock}>
@@ -23,12 +30,21 @@ export const UserHeader = () => {
                 </div>
                 <div className={styles.actionsButtonBlock}>
                     <div className={styles.actionBlock}>
-                        <Button buttonColor={'clearButton'}>
-                            <span className={styles.btnText}>Subscribe</span>
-                        </Button>
+                        {isSubscribe
+                            ?
+                            <Button onClick={() => setSubscribe(false)}>
+                                <span>Subscribed</span>
+                            </Button>
+                            :
+                            <Button buttonColor={'clearButton'} onClick={() => setSubscribe(true)}>
+                                <span className={styles.btnText}>Subscribe</span>
+                            </Button>
+                        }
+
+
                     </div>
                     <div className={styles.actionBlock}>
-                        <Button buttonColor={'clearButton'}>
+                        <Button buttonColor={'clearButton'} onClick={() => navigate(MESSAGES)}>
                             <span className={styles.btnText}>Message</span>
                         </Button>
                     </div>
